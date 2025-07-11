94423b6e6aed852269b013e11d35a793fa82a092
AnirudhSaketh <156160852+AnirudhSaketh@users.noreply.github.com>
2025-07-11 10:11:05 +0530

    Create tuple.rs

---
+++ tuple.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let my_tuple = (10, 2.5, 'A');
+
+    let x = my_tuple.0;
+    let y = my_tuple.1;
+    let z = my_tuple.2;
+
+    println!("x = {}", x);
+    println!("y = {}", y);
+    println!("z = {}", z);
+}
